22184dbb35efed25b9639a7ea73643e88c2bf1ad
Helga Alvarez <helgaalvarez@gmail.com>
2012-02-13 14:31:18 -0600

    finishing source code for Localizing strings and images

--- iOS/LocalizeMe/LocalizeMe/ViewController.m
+++ iOS/LocalizeMe/LocalizeMe/ViewController.m
@@ -26,9 +26,19 @@
 
 - (void)viewDidLoad
 {
+    // NSLocale works similar to a Dictionary
+    NSLocale *locale = [NSLocale currentLocale]; 
+    NSString *displayNameString = [locale 
+                                   displayNameForKey:NSLocaleIdentifier value:[locale localeIdentifier]];
+    localizableLabel.text = displayNameString;
+//    label1.text = NSLocalizedString(@"One", @"The number 1");
+//    label2.text = NSLocalizedString(@"Two", @"The number 2");
+//    label3.text = NSLocalizedString(@"Three", @"The number 3");
+//    label4.text = NSLocalizedString(@"Four", @"The number 4");
+//    label5.text = NSLocalizedString(@"Five", @"The number 5");
+    
     [super viewDidLoad];
-    NSLocale *locale = [NSLocale currentLocale];
-    NSString *displayNameString = [locale displayNameForKey:NSLocaleIdentifier value:[locale localeIdentifier]]
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -40,7 +50,9 @@
     [self setLabel3:nil];
     [self setLabel4:nil];
     [self setLabel5:nil];
+    
     [super viewDidUnload];
+    
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
@@ -72,12 +84,14 @@
 }
 
 - (void)dealloc {
+    
     [localizableLabel release];
     [label1 release];
     [label2 release];
     [label3 release];
     [label4 release];
     [label5 release];
+    
     [super dealloc];
 }
 @end
